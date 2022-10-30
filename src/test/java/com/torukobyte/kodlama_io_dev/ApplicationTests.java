1afa71b85f5f1682f18ca18fc797b8b61c848e29
torukobyte <torukobyte@gmail.com>
2022-10-30 03:29:50 +0300

    Project created

---
+++ src/test/java/com/torukobyte/kodlama_io_dev/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.torukobyte.kodlama_io_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
